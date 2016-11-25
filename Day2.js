201e7ada5ade8d6e0f628033c36eb762b019771b
luoyican <463797482@qq.com>
2016-11-25 11:58:04 +0800

    屏保

--- Day2.js
+++ Day2.js
@@ -9,66 +9,142 @@ import {
     View,
     TouchableOpacity,
     AppRegistry,
+    Image,
 } from 'react-native';
 
-class App extends React.Component {
-    constructor(props){
+class Day2 extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            value:"click me",
-            isfirst:true,
-            timeLeft:15,
+        console.disableYellowBox = true;
+        this.state = {
+            isfirst: true,
+            timeLeft: 15,
+            value: "倒计时" + 15 + "秒",
+            iconBG: require('./img/Koala.jpg'),
+            pressTime: 0,
+        }
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    changeBG() {
+        let i = this.state.pressTime % 8;
+        this.state.pressTime += 1;
+        switch (i) {
+            case 0:
+                this.setState({
+                    iconBG: require('./img/Hydrangeas.jpg'),
+                });
+                break;
+            case 1:
+                this.setState({
+                    iconBG: require('./img/Desert.jpg'),
+                });
+                break;
+            case 2:
+                this.setState({
+                    iconBG: require('./img/Jellyfish.jpg'),
+                });
+                break;
+            case 3:
+                this.setState({
+                    iconBG: require('./img/Koala.jpg'),
+                });
+                break;
+            case 4:
+                this.setState({
+                    iconBG: require('./img/Chrysanthemum.jpg'),
+                });
+                break;
+            case 5:
+                this.setState({
+                    iconBG: require('./img/Lighthouse.jpg'),
+                });
+                break;
+            case 6:
+                this.setState({
+                    iconBG: require('./img/Penguins.jpg'),
+                });
+                break;
+            case 7:
+                this.setState({
+                    iconBG: require('./img/Tulips.jpg'),
+                });
+
         }
     }
 
-    componentDidMount(){
+    _onPress() {
+        this.changeBG();
+    }
+
+    _onBegin() {
+        this.timer && clearInterval(this.timer);
+
+        this.timer = setInterval(()=> {
+            if (this.state.timeLeft > 0) {
+                this.changeBG();
+                this.setState({
+                    timeLeft: --this.state.timeLeft,
+                    value: "倒计时" + this.state.timeLeft + "秒",
+                })
+            } else {
+                this.timer && clearInterval(this.timer);
+                this.setState({
+                    value: "倒计时完成咯，精彩继续",
+                });
+            }
+        }, 500);
 
     }
 
-    componentWillUnmount(){
-
-    }
-
-    _onPress(){
-        if(this.state.isfirst){
-            this.setState({
-                value:" pless don't do that ,press me",
-                isfirst:false,
-            });
-        }else{
-            this.setState({
-                value:" press me",
-                isfirst:true,
-            });
-        }
-    }
-
-    _onTime(){
-        if(this.state.isfirst){
-            this.timer = setInterval(()=>{this.setState({timeLeft: --this.state.timeLeft,})},3000);
-            isfirst=false;
-        }else{
+    _onStop() {
+        if (this.state.timeLeft > 10) {
+            console.log('this.state.timeLeft > 10');
             this.timer && clearInterval(this.timer);
-            isfirst=true;
+        } else {
+            console.log('this.state.timeLeft <= 10');
+            this.setState({
+                timeLeft: this.state.timeLeft * 2 + 11,
+            });
         }
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={()=>this._onPress()}>
-                    <Text style={styles.instructions}>
-                        {this.state.value}
-                    </Text>
-                </TouchableOpacity>
+                <Image source={this.state.iconBG}
+                       style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center',}}>
+                    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center',}}>
+                        <Image source={this.state.iconBG} style={{height: 100, width: 100,}}/>
 
+                        <TouchableOpacity onPress={()=>this._onPress()}>
+                            <Text style={styles.instructions}>
+                                更改背景图
+                            </Text>
+                        </TouchableOpacity>
 
-                <TouchableOpacity onPress={()=>this._onTime()}>
-                    <Text style={styles.instructions}>
-                        点我开始倒计时15秒
-                    </Text>
-                </TouchableOpacity>
-                <Text>倒计时{this.state.timeLeft}秒</Text>
+                        <TouchableOpacity onPress={()=>this._onStop()}>
+                            <Text style={styles.instructions}>
+                                Pause or Reset
+                            </Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={()=>this._onBegin()}>
+                            <Text style={styles.instructions}>
+                                点我开始倒计时{this.state.timeLeft}秒
+                            </Text>
+                        </TouchableOpacity>
+                        <Text>{this.state.value}</Text>
+                    </View>
+                </Image>
             </View>
         );
     }
@@ -85,6 +161,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         textAlign: 'center',
         margin: 15,
+        color: 'red',
     },
 });
 
